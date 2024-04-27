3455df6996c0411491fef88c513deb2223bb93ab
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-04-27 09:10:28 +0200

    correction icons skills

--- components/homeComponents/skills/components/SkillCard.tsx
+++ components/homeComponents/skills/components/SkillCard.tsx
@@ -4,12 +4,12 @@ type SkillCardProps = {
   skill: SkillsListType;
 };
 
-export default function SkillCard({skill}: SkillCardProps) {
-
+export default function SkillCard({ skill }: SkillCardProps) {
   return (
     <div className="w-[94vw] md:max-w-[310px] mx-auto border-2 border-ring p-2 xs:p-3 sm:p-4 flex flex-col gap-y-4 rounded-lg  bg-card text-card-foreground customShadow">
       <div className="flex justify-between items-center mb-4 ">
-        <h2 className="text-primary">{skill.icon}</h2><h3 className="text-primary">{skill.label}</h3>
+        <h2 className="text-primary m-0">{skill.icon}</h2>
+        <h3 className="text-primary">{skill.label}</h3>
       </div>
       <p>{skill.text}</p>
     </div>
