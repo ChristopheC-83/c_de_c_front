9832eb25758fe1954bdfb3c79fad728b3c6ebcd9
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-05-01 14:47:10 +0200

    dynamic site ok

--- datas/learn-more/dynamic-site.tsx
+++ datas/learn-more/dynamic-site.tsx
@@ -5,6 +5,7 @@ import { AiFillPicture } from "react-icons/ai";
 import { FaComputer } from "react-icons/fa6";
 import { FaFlagCheckered } from "react-icons/fa6";
 import { GiThreeFriends } from "react-icons/gi";
+import { SiAwsorganizations } from "react-icons/si";
 
 export const dynamicSiteList: SkillsListType[] = [
   {
@@ -14,10 +15,12 @@ export const dynamicSiteList: SkillsListType[] = [
     text: (
       <>
         <p>Nous les déterminons ensemble.</p>
-        <p>Autant le visuel du site vitrine...</p>
-        <p>... que les fonctinnalités du site d'administration</p>
-        <p>Je vous fournis une prestation détaillée. </p>
-        <p>Vous validez ou nous modifions.</p>
+        <p>Autant le visuel et les fonctionnalités du site vitrine...</p>
+        <p>... que les options du site d'administration</p>
+        <p>Je vous fournis une prestation détaillée possible. </p>
+        <p>
+          Vous validez ou nous modifions, ajustons au plus près de vos besoins..
+        </p>
         <br />
         <p className="text-center">
           <b>On détaille au maximum pour un efficacité optimale !</b>
@@ -33,10 +36,45 @@ export const dynamicSiteList: SkillsListType[] = [
       <>
         <p>Quel style souhaitez-vous renvoyer ?</p>
         <p>Les images ? Le menu ?</p>
-        <p>L'accueil ? une image ou un carroussel ?</p>
+        <p>L' accueil : une image ou un carroussel ?</p>
+        <p>Les utilisateurs : visiteurs libres ou connectés ?</p>
+        <p>Les produits : une liste ou une grille ?</p>
         <br />
         <p className="text-center">
-          <b>Regardons ça ensemble!</b>
+          <b>
+            Les possibilités sont presque infinies ! Mais je suis là pour vous
+            accompagner !
+          </b>
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 99,
+    icon: <SiAwsorganizations />,
+    label: "Vos besoins de gestion",
+    text: (
+      <>
+        <p>
+          Quel est votre coeur de métier qui doit être au centre de votre
+          management des données ?
+        </p>
+        <p>
+          Quels affichages (textes, images, vidéos) souhaitez-vous faire évoluer
+          avec le temps?
+        </p>
+        <p>L'accueil : une image ou un carroussel ?</p>
+        <p>
+          Serez-vous seul maître à bord ?Ou accompagné de collaborateurs ? dans
+          ce cas, quelle gestion des droits met-on en place ?{" "}
+        </p>
+        <p>Les produits : une liste ou une grille ?</p>
+        <br />
+        <p className="text-center">
+          <b>
+            Avec votre connaissance métier et mes 15 ans de commerce et 10 ans
+            de services, nous allons vous créer un outil de gestion sur-mesure !
+          </b>
         </p>
       </>
     ),
@@ -47,12 +85,14 @@ export const dynamicSiteList: SkillsListType[] = [
     label: "Le développement",
     text: (
       <>
-        <p>Tout est cadré !?!</p>
-        <p>C'est à moi de jouer !</p>
+        <p>Tout est cadré !?! C'est à moi de jouer !</p>
         <p>Vous avez des rendus réguliers de l'avancement du projet.</p>
+        <p>Vous testez, consolidez, ajustez si besoins sur l'outil de gestion.</p>
+        <p>Vous validez le visuel du site client.</p>
         <br />
         <p className="text-center">
-          <b>Nous ajustons le cap au fun et à mesure !</b>
+          <b>Nous ajustons le cap au fun et à mesure !</b><br/>
+          Pas besoin de changement brusque, nous avions préparé votre projet dès le début !
         </p>
       </>
     ),
@@ -64,6 +104,7 @@ export const dynamicSiteList: SkillsListType[] = [
     text: (
       <>
         <p>On y est !</p>
+        <p>Je vous accompagne dans la prise en main de l'outil.</p>
         <p>Si tout est validé, à votre convenance :</p>
         <p>
           1- Le code vous appartient, libre à vous de l'héberger ou bon vous
@@ -83,12 +124,12 @@ export const dynamicSiteList: SkillsListType[] = [
     label: "Et après ?",
     text: (
       <>
-        <p>Vous n'êtes pas qu'un client !</p>
-        <p>Vous m'avez fait confiance ?</p>
-        <p>Je reste disponible !</p>
+        <p>Vous n'êtes pas qu'un client ! Vous m'avez fait confiance ?</p>
+        <p>Je reste disponible ! </p>
+        <p>Vos besoins évoluent, vos envies changent... je suis là !</p>
         <br />
         <p className="text-center">
-          <b>Une correction ? Une mise à jour ? je serai là !</b>
+          <b>Une correction ? Une mise à jour ? je serai encore et toujours présent !</b>
         </p>
       </>
     ),
