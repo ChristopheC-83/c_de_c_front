45120cd4e7be95b4fc106e2960b88355d5f0aeed
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-05-22 17:22:51 +0200

    test !?!

--- components/homeComponents/hero/Hero.tsx
+++ components/homeComponents/hero/Hero.tsx
@@ -10,12 +10,11 @@ export default function Hero() {
           <h2 className="text-clip  mb-8">Développeur Web</h2>
           <p>
             Je suis à la recherche de la meilleure solution aux problématiques
-            rencontrées en embrassant avec enthousiasme de nouvelles
-            technologies si nécessaire !
+            rencontrées en utilisant la techno la plus efficace !
           </p>
           <p className="text-center text-2xl">&</p>
           <p className="text-end">
-            Je souhaite apporter ma "petite pierre" dans le parcours des étudiants
+            Je souhaite apporter ma petite pierre dans le parcours des étudiants
             en tant que{" "}
             <span className="text-clip  text-nowrap	">Compagnon de Code</span>.
           </p>
